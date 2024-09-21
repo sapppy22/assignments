e4601685510127f10023106aec72ec0abce11184
sapppy22 <137591541+sapppy22@users.noreply.github.com>
2024-09-22 04:16:54 +0530

    Update 3-promise-all.js

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -4,20 +4,36 @@
  * Return a promise.all which return the time in milliseconds it takes to complete the entire operation.
  */
 
-function wait1(t) {
-
+function wait1(t1) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("wait t1 is resolved");
+    }, t1 * 1000);
+  });
 }
 
-function wait2(t) {
-
+function wait2(t2) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("wait t2 is resolved");
+    }, t2 * 1000);
+  });
 }
 
-function wait3(t) {
-
+function wait3(t3) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("wait t3 is resolved");
+    }, t3 * 1000);
+  });
 }
 
 function calculateTime(t1, t2, t3) {
-
+  const start = Date.now();
+  return Promise.all([wait1, wait2, wait3]).then(() => {
+    const end = Date.now();
+    return end - start;
+  });
 }
+module.exports = { wait1, wait2, wait3, calculateTime };
 
-module.exports = calculateTime;
