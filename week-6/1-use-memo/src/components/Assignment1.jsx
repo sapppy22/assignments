5dd40a58a45a403b4082f15986da0e6408cf8d1a
sapppy22 <137591541+sapppy22@users.noreply.github.com>
2025-05-20 04:02:41 +0530

    Solved Assignment1.jsx

--- week-6/1-use-memo/src/components/Assignment1.jsx
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -6,7 +6,14 @@ import { useState } from "react";
 export function Assignment1() {
     const [input, setInput] = useState(0);
     // Your solution starts here
-    const expensiveValue = 0; 
+    
+    const expensiveValue= useMemo(()=>{
+       let c=1;
+       for(int i=input;i>=1;i--){
+          c=c*i;
+        }
+       return c;
+    },[input]);
     // Your solution ends here
 
     return (
